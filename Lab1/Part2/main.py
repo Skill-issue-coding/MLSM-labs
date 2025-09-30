952287c3545c9fab8949f0e5588a4b4251729309
Emil Djurson <emidj236@student.liu.se>
2025-09-30 18:40:52 +0200

    Basic clutering based on instructions

--- Lab1/Part2/main.py
+++ Lab1/Part2/main.py
@@ -1,15 +1,19 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import AgglomerativeClustering
 
+# Reading data
 SHOPPING_DATA_URL = 'Lab1/Part2/shopping_data.csv'
 customer_data = pd.read_csv(SHOPPING_DATA_URL)
 
+# Dropping irrelevant information
 customer_data = customer_data.drop(['CustomerID', 'Genre', 'Age'], axis=1)
-
 data = customer_data[['Annual Income (k$)', 'Spending Score (1-100)']].values
 
-print(data)
+# print(data)
 
+# Plotting original data
 labels = range(1,201)
 plt.figure(figsize=(10,7))
 plt.xlabel('Annual Income')
@@ -18,6 +22,15 @@ plt.subplots_adjust(bottom=0.1)
 plt.scatter(data[:,0], data[:,1], label='True Position')
 plt.show()
 
-"""
-    Create clusters
-"""
+# Dendogram
+linked = linkage(data, 'single')
+labelList = range(1,201)
+plt.figure(figsize=(10,7))
+dendrogram(linked, orientation='top', labels=labelList, distance_sort='descending', show_leaf_counts=True)
+plt.show()
+
+# Clustering
+cluster = AgglomerativeClustering(n_clusters=5, linkage='ward')
+cluster.fit_predict(data)
+plt.scatter(data[:,0],data[:,1], c=cluster.labels_, cmap='rainbow')
+plt.show()
