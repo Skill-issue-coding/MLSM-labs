0037c5de9c3239ac601746065cc45fdb5b47d101
AntonMartic <anton.martic@gmail.com>
2025-10-13 20:30:15 +0200

    SVM

--- Lab2/Part3/main.py
+++ Lab2/Part3/main.py
@@ -5,33 +5,37 @@ from scipy import stats
 import seaborn as sns; sns.set()
 
 from sklearn.datasets import make_blobs
-X, y = make_blobs(n_samples=50, centers=2, random_state=0, cluster_std=0.60)
 
-'''
+from sklearn.svm import SVC
+
+""" Motivating Support Vector Machines """
+X, y = make_blobs(n_samples=50, centers=2, random_state=0, cluster_std=0.60)
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
-plt.show()
-'''
-'''
+# plt.show()
+
+"""
 xfit = np.linspace(-1, 3.5)
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
 plt.plot([0.6], [2.1], 'x', color='red', markeredgewidth=2, markersize=10)
 for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
- plt.plot(xfit, m * xfit + b, '-k')
+    plt.plot(xfit, m * xfit + b, '-k')
 plt.xlim(-1, 3.5)
 plt.show()
-'''
-'''
+"""
+
+""" Maximizing the Margin 
 xfit = np.linspace(-1, 3.5)
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
 for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]:
- yfit = m * xfit + b
- plt.plot(xfit, yfit, '-k')
- plt.fill_between(xfit, yfit - d, yfit + d, edgecolor='none', color='#AAAAAA', alpha=0.4)
+    yfit = m * xfit + b
+    plt.plot(xfit, yfit, '-k')
+    plt.fill_between(xfit, yfit - d, yfit + d, edgecolor='none', color='#AAAAAA', alpha=0.4)
 plt.xlim(-1, 3.5)
 plt.show()
-'''
+"""
 
-from sklearn.svm import SVC
+
+""" Fitting a support vector machine """
 model = SVC(kernel='linear', C=1E10)
 model.fit(X, y)
 
@@ -41,32 +45,29 @@ def plot_svc_decision_function(model, ax=None, plot_support=True):
         ax = plt.gca()
     xlim = ax.get_xlim()
     ylim = ax.get_ylim ()
-
     # create grid to evaluate model
     x = np.linspace(xlim[0], xlim[1], 30)
     y = np.linspace(ylim[0], ylim[1], 30)
     Y, X = np.meshgrid(y, x)
     xy = np.vstack([X.ravel(), Y.ravel()]).T
     P = model.decision_function(xy).reshape(X.shape)
-
     # plot decision boundary and margins
     ax.contour(X, Y, P, colors='k',
     levels=[-1, 0, 1], alpha=0.5,
     linestyles=['--', '-', '--'])
-
     # plot support vectors
     if plot_support:
         ax.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1],
         s=300, linewidth=1, facecolors='none')
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
+
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
 plot_svc_decision_function(model)
 plt.show()
 
-'''
-print(model.support_vectors_)
-'''
+#print(model.support_vectors_)
+
 def plot_svm(N=10, ax=None):
     X, y = make_blobs(n_samples=200, centers=2, random_state=0, cluster_std=0.60)
     X = X[:N]
@@ -78,7 +79,6 @@ def plot_svm(N=10, ax=None):
     ax.set_xlim(-1, 4)
     ax.set_ylim(-1, 6)
     plot_svc_decision_function(model, ax)
-
 fig, ax = plt.subplots(1, 2, figsize=(16, 6))
 fig.subplots_adjust(left=0.0625, right=0.95, wspace=0.1)
 for axi, N in zip(ax, [60, 120]):
@@ -87,12 +87,8 @@ for axi, N in zip(ax, [60, 120]):
 plt.show()
 
 
-from sklearn.datasets import make_circles
-X, y = make_circles(100, factor=.1, noise=.1)
-clf = SVC(kernel='linear').fit(X, y)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
-plot_svc_decision_function(clf, plot_support=False)
-plt.show()
+""" Beyond linear boundaries: Kernel SVM """
+
 
 # 1. What is the basic idea/intuition of SVM?
 # The basic idea behind Support Vector Machines (SVM) is to find the best separating
