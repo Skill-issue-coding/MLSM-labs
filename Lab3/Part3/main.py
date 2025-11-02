c4aadfc65eb235c934a174f327bb17a260f4a24e
AntonMartic <anton.martic@gmail.com>
2025-11-02 16:48:07 +0100

    Car Evaluation Quality

--- Lab3/Part3/main.py
+++ Lab3/Part3/main.py
@@ -1,4 +1,4 @@
-""" Validation Metrics """
+""" Validation Metrics 
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import cross_val_score
@@ -18,9 +18,9 @@ data_url = "http://lib.stat.cmu.edu/datasets/boston"
 raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
 data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
 target = raw_df.values[1::2, 2]
+"""
 
-
-""" Regression """
+""" Regression 
 #from sklearn.datasets import load_boston
 #boston = load_boston()
 X=data
@@ -82,9 +82,9 @@ scores = cross_val_score(knn, X, Y, cv=cv)
 print("mean R2: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 predicted = cross_val_predict(knn, X,Y, cv=cv)
 print("MSE: %0.2f" % mean_squared_error(Y,predicted))
+"""
 
-
-""" Recursive Feature Elimination (RFE) """
+""" Recursive Feature Elimination (RFE) 
 
 from sklearn.feature_selection import RFE
 best_features=4
@@ -142,4 +142,82 @@ score = cross_val_score(svm_lin, X[:, mask], Y, cv=cv)
 print("mean R2: %0.2f (+/- %0.2f)" % (score.mean(), score.std() * 2))
 predicted = cross_val_predict(svm_lin, X[:, mask], Y, cv=cv)
 print("MSE: %0.2f" % mean_squared_error(Y, predicted))
+"""
 
+""" Car Evaluation Quality """
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn import svm
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+df = pd.read_csv('data_cars.csv', header=None)
+for i in range(len(df.columns)):
+    df[i] = df[i].astype('category')
+print(df.head())
+
+# map categories to values
+map0 = dict(zip(df[0].cat.categories, range(len(df[0].cat.categories))))
+map1 = dict(zip(df[1].cat.categories, range(len(df[1].cat.categories))))
+map2 = dict(zip(df[2].cat.categories, range(len(df[2].cat.categories))))
+map3 = dict(zip(df[3].cat.categories, range(len(df[3].cat.categories))))
+map4 = dict(zip(df[4].cat.categories, range(len(df[4].cat.categories))))
+map5 = dict(zip(df[5].cat.categories, range(len(df[5].cat.categories))))
+map6 = dict(zip(df[6].cat.categories, range(len(df[6].cat.categories))))
+
+cat_cols = df.select_dtypes(['category']).columns
+df[cat_cols] = df[cat_cols].apply(lambda x: x.cat.codes)
+df = df.iloc[np.random.permutation(len(df))]
+print(df.head())
+
+df_f1 = pd.DataFrame(columns=['method'] + sorted(map6, key=map6.get))
+df_precision = pd.DataFrame(columns=['method'] + sorted(map6, key=map6.get))
+df_recall = pd.DataFrame(columns=['method'] + sorted(map6, key=map6.get))
+
+
+def CalcMeasures(method, y_pred, y_true, df_f1=df_f1, df_precision=df_precision, df_recall=df_recall):
+    df_f1.loc[len(df_f1)] = [method] + list(f1_score(y_true, y_pred, average=None, zero_division=0))
+    df_precision.loc[len(df_precision)] = [method] + list(precision_score(y_true, y_pred, average=None, zero_division=0))
+    df_recall.loc[len(df_recall)] = [method] + list(recall_score(y_true, y_pred, average=None, zero_division=0))
+
+
+X = df[df.columns[:-1]].values
+Y = df[df.columns[-1]].values
+
+cv = 10
+method = 'linear support vector machine'
+clf = svm.SVC(kernel='linear', C=50)
+y_pred = cross_val_predict(clf, X, Y, cv=cv)
+CalcMeasures(method, y_pred, Y)
+
+method = 'naive bayes'
+clf = MultinomialNB()
+y_pred = cross_val_predict(clf, X, Y, cv=cv)
+CalcMeasures(method, y_pred, Y)
+
+method = 'logistic regression'
+clf = LogisticRegression()
+y_pred = cross_val_predict(clf, X, Y, cv=cv)
+CalcMeasures(method, y_pred, Y)
+
+method = 'k nearest neighbours'
+clf = KNeighborsClassifier(weights='distance', n_neighbors=5)
+y_pred = cross_val_predict(clf, X, Y, cv=cv)
+CalcMeasures(method, y_pred, Y)
+
+print("\nf-measure:")
+print(df_f1)
+print("\nPrecision:")
+print(df_precision)
+print("\nRecall:")
+print(df_recall)
+
+labels_counts = df[6].value_counts()
+print("\nNumber of samples in each class:")
+print(pd.Series(map6).map(labels_counts))
