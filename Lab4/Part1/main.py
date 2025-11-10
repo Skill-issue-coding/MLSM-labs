55d99101172c6ec0ea6aae4b81e50409dc96fe1c
AntonMartic <anton.martic@gmail.com>
2025-11-10 11:10:00 +0100

    Start lab 4

---
+++ Lab4/Part1/main.py
@@ -0,0 +1,7 @@
+""" Text feature extraction TF-IDF """
+
+d1 = "The sky is blue."
+d2 = "The sun is bright."
+d3 = "The sun in the sky is bright."
+d4 = "We can see the shining sun, the bright sun."
+Z = (d1,d2,d3,d4)
